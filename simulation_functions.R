29757c5087ca92d5257329eeef65eaf9d541dfa3
Kris Sankaran <sankaran.kris@gmail.com>
2021-04-17 18:46:31 -0500

    functional equivalence simulation

--- simulation_functions.R
+++ simulation_functions.R
@@ -5,6 +5,13 @@ k_topics <- function(K, V = 500, lambda = 1) {
     widen_betas()
 }
 
+widen_betas <- function(betas) {
+  betas %>%
+    pivot_wider(m:k_LDA, names_from = "w", values_from = "b") %>%
+    mutate(k = as.numeric(as.factor(k_LDA))) %>%
+    select(-K, -k_LDA)
+}
+
 simulate_lda <- function(betas, gammas, n0=NULL) {
   n <- nrow(gammas)
   if (is.null(n0)) {
@@ -19,6 +26,63 @@ simulate_lda <- function(betas, gammas, n0=NULL) {
   colnames(x) <- seq_len(ncol(betas))
   x
 }
+
+#' Simulation functions for functional equivalence experiment
+
+perturb_topic <- function(beta_k, species_subsets, nu=2) {
+  beta_k[species_subsets[[1]]] <-  nu * beta_k[species_subsets[[1]]]
+  beta_k[species_subsets[[2]]] <-  (1 / nu) * beta_k[species_subsets[[2]]]
+  beta_k / sum(beta_k)
+}
+
+perturb_topics <- function(B, n_per_topic = NULL, subset_size = 30, nus=c(1, 4)) {
+  K <- nrow(B)
+  stopifnot(subset_size < ncol(B))
+  if (is.null(n_per_topic)) {
+    n_per_topic <- c(rep(20, 2), rep(1, K - 2))
+  }
+  
+  B_tilde <- vector(length = K, mode = "list")
+  for (k in seq_len(K)) {
+    s <- list(1:(subset_size / 2), (subset_size / 2 + 1):subset_size)
+    class_k <- matrix(nrow = n_per_topic[k], ncol = ncol(B))
+    nu <- runif(n_per_topic[k], nus[1], nus[2])
+    for (i in seq_len(n_per_topic[k])) {
+      class_k[i, ] <- perturb_topic(B[k, ], s, nu[i])
+    }
+    B_tilde[[k]] <- as_tibble(class_k)
+  }
+  
+  bind_rows(B_tilde, .id = "k") %>%
+    pivot_longer(-k, names_to = "w", values_to = "b")
+}
+
+equivalence_data <- function(N, V, K, lambdas, n0 = NULL, ...) {
+  if (is.null(n0)) {
+    n0 <- rpois(N, 1000)
+  }
+  
+  B <- rdirichlet(K, rep(lambdas$beta, V))
+  B_tilde <- perturb_topics(B, ...)
+  gammas <- rdirichlet(N, rep(lambdas$gamma, K))
+  x <- matrix(nrow = N, ncol = V)
+  
+  for (i in seq_len(N)) {
+    B_i <- B_tilde %>%
+      group_by(k, w) %>%
+      sample_n(1) %>%
+      pivot_wider(k, names_from = "w", values_from = "b") %>%
+      ungroup() %>%
+      select(-k) %>%
+      as.matrix()
+    x[i, ] <- rmultinom(1, n0[i], t(B_i) %*% gammas[i, ])
+  }
+  
+  colnames(x) <- seq_len(ncol(x))
+  rownames(x) <- seq_len(nrow(x))
+  x
+}
+
 
 .simulate_replicate <- function(betas, gammas, M = 5, mechanism = NULL) {
   if (is.null(mechanism)) {
