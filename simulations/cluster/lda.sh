397689a1a0a1e94fffc4207379f0e397dbf0c381
Kris Sankaran <sankaran.kris@gmail.com>
2022-01-11 22:07:46 -0600

    Can clone repo, now that it is public

--- simulations/cluster/lda.sh
+++ simulations/cluster/lda.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-cp /srv/rlibs/alto/doc/*Rmd .
+git clone git@github.com:lasy/alto.git
+cd alto/vignettes/
 
 for (( k = 0; k < 10; ++k )); do
   export RUN=$( expr 10 '*' "$id" + "$k")
-  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='product', N=250, V=1000, n_models=10))"
-  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='transport', N=250, V=1000, n_models=10))"
+  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='product', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
+  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='transport', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
 done;
 
 tar -zcvf lda-$id.tar.gz lda/
