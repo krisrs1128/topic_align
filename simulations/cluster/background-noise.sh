a4b22d4da7646181dbbd781ee2264e2bdcf06c2d
Kris Sankaran <sankaran.kris@gmail.com>
2022-01-31 18:42:29 -0600

    give K

--- simulations/cluster/background-noise.sh
+++ simulations/cluster/background-noise.sh
@@ -5,8 +5,8 @@ cp /alto/vignettes/*.Rmd .
 for (( k = 0; k < 1; ++k )); do
   export RUN=$( expr 1 '*' "$id" + "$k")
   for alpha in $(seq 0.0 0.1 1); do
-    Rscript -e "rmarkdown::render('background-noise.Rmd', params=list(id = $RUN, alpha=$alpha, method='product', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
-    Rscript -e "rmarkdown::render('background-noise.Rmd', params=list(id = $RUN, alpha=$alpha, method='transport', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
+    Rscript -e "rmarkdown::render('background-noise.Rmd', params=list(id = $RUN, alpha=$alpha, method='product', K=5, N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
+    Rscript -e "rmarkdown::render('background-noise.Rmd', params=list(id = $RUN, alpha=$alpha, method='transport', K=5, N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
   done;
 done;
 
--- simulations/cluster/lda.sh
+++ simulations/cluster/lda.sh
@@ -4,8 +4,8 @@ cp /alto/vignettes/*.Rmd .
 
 for (( k = 0; k < 10; ++k )); do
   export RUN=$( expr 10 '*' "$id" + "$k")
-  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='product', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
-  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='transport', N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
+  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='product', K=5, N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
+  Rscript -e "rmarkdown::render('lda.Rmd', params=list(id = $RUN, method='transport', K=5, N=250, V=1000, n_models=10, save = TRUE, perplexity = TRUE))"
 done;
 
 tar -zcvf lda-$id.tar.gz lda/
